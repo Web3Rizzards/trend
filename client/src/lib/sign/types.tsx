5e8264bdf0e9e5dd12ee4fe1bd1af40393dc3d93
Kenk <kenghin_lim@hotmail.com>
2024-09-21 17:33:10 +0800

    feat(client): add sign packages

---
+++ client/src/lib/sign/types.tsx
@@ -0,0 +1,13 @@
+export type PersonType = {
+    name: string;
+    preferredUsername: string;
+};
+
+export type ReactionType = {
+    reactionType: string;
+};
+
+export type PostType = {
+    content: string;
+    image: string;
+};
